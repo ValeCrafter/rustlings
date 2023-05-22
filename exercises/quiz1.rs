f3fc62185da7c928fdea5741a42efb427b5a02c1
ValeCrafter <71349271+ValeCrafter@users.noreply.github.com>
2023-05-22 07:20:22 +0000

    modified:   exercises/functions/functions1.rs 	modified:   exercises/functions/functions2.rs 	modified:   exercises/functions/functions3.rs 	modified:   exercises/functions/functions4.rs 	modified:   exercises/functions/functions5.rs 	modified:   exercises/if/if1.rs 	modified:   exercises/if/if2.rs 	modified:   exercises/intro/intro1.rs 	modified:   exercises/intro/intro2.rs 	modified:   exercises/quiz1.rs 	modified:   exercises/variables/variables1.rs 	modified:   exercises/variables/variables2.rs 	modified:   exercises/variables/variables3.rs 	modified:   exercises/variables/variables4.rs 	modified:   exercises/variables/variables5.rs 	modified:   exercises/variables/variables6.rs 	new file:   rustlings

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,7 +13,9 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+ fn calculate_price_of_apples(amt: i32) -> i32 {
+    
+ }
 
 // Don't modify this function!
 #[test]
